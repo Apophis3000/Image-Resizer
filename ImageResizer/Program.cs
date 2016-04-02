039a4a289e2edf684034ae38adb75e5bf9269842
Philipp Engelmann <pengelmann92@gmail.com>
2016-04-02 06:58:07 +0200

    - push initial repo

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Bild.jpg";
+            Bitmap res = new Bitmap(path);
+
+            //Bitmap res = Rescale(path, 1200, 750);
+            res.Save(@"C:\Projects\neu.jpg", ImageFormat.Jpeg);
+        }
+
+        static Bitmap Rescale(string path, int newWidth, int newHeight)
+        {
+            Bitmap bmp = new Bitmap(path, true);
+
+            int oldWidth = bmp.Width;
+            int oldHeight = bmp.Height;
+
+            Bitmap result = new Bitmap(oldWidth, oldHeight);
+
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.DrawImage(result, 0, 0);
+            g.Dispose();
+            return result;
+        }
+
+    }
+}
